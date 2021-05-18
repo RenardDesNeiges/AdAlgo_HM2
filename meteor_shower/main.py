4ebc3fcf7ef53bafb1dc67f14373cc5e86c6e00a
Titouan Renard <titouan.renard@gmail.com>
2021-05-18 11:46:25 +0200

    c + 1 implemented, it seems a bit slow

--- meteor_shower/main.py
+++ meteor_shower/main.py
@@ -11,10 +11,10 @@ import time
 def contract(E,ec,V):
     v1 = ec[0]
     v2 = ec[1]
-    # contraction procedure over edgelist
+    # contraction procedure over edgelist (we iterate backwards over the list so deletion is seemless)
     for i in range(len(E) - 1, -1, -1):
         e = E[i]
-        if e == ec or e == [v2,v1]:
+        if (e[0] == v1 and e[1] == v2) or (e[1] == v1 and e[0] == v2) :
             del E[i]
             continue
         if e[0] == v2:
@@ -26,7 +26,7 @@ def contract(E,ec,V):
 
 # modified Karger-Stein algorithm
 # note, deepcopy the graph before calling the function because it will modify the original datastructures
-def modKargStein(E,V,n,alpha,C):
+def modKargStein(E,V,n,alpha):
     if n == 3 : 
         # pick cut uniformly at random in V choose 2
         i = random.randrange(3)
@@ -40,16 +40,17 @@ def modKargStein(E,V,n,alpha,C):
             ec = E[random.randrange(len(E))]    # choose an edge uniformly at random 
             contract(E,ec,V)                    # contract it in G
         
-        # run the procedure recursively twice
+        # Call ModKargStein twice on the output of the procedure so far
         E1 = copy.deepcopy(E)
         V1 = copy.deepcopy(V)
-        modKargStein(E1, V1, len(V1), alpha, C)
+        modKargStein(E1, V1, len(V1), alpha)
         E2 = copy.deepcopy(E)
         V2 = copy.deepcopy(V)
-        modKargStein(E2, V2, len(V2), alpha, C)
-        # print([len(E1),len(E2)])
-        return(min(len(E1),len(E2)))
-        # Call ModKargStein twice on the output of the procedure so far, return the critical sets that are C or C+1
+        modKargStein(E2, V2, len(V2), alpha)
+
+        # return the critical sets 
+        return E1, E2 #min(len(E1),len(E2))
+        
 
 """
     Parsing the input 
@@ -78,23 +79,48 @@ for line in sys.stdin:
     else:
         e1 = int(lst[0])
         e2 = int(lst[1])
-        E.append([e1,e2])
+        # edge reprensentaiton is a bit shady : we use ((v1,v2,v1,v2)) where the first tuple will be affected by contraction but not the second (which allows to keep track of cuts that are obtained)
+        E.append([e1,e2,e1,e2])
 
     lnb = lnb + 1
 
-minC = m
+C = m
 
 O = 1
 start = time.time()
 
+critsets = []
+
 ## find a min-cut 
-for i in range(O * math.ceil(math.pow(n,3)*math.log(n))):
-# while time.time() - start < 3.8:
+# for i in range(O * math.ceil(math.pow(n,3)*math.log(n))):
+while time.time() - start < 3.8:
     Ei = copy.deepcopy(E)
     Vi = copy.deepcopy(V)
-    nmc = modKargStein(Ei,Vi,n,1,m)
+    E1, E2 = modKargStein(Ei,Vi,n,1)
+    nmc = min(len(E1),len(E2))
 
-    if nmc < minC:
-        minC = nmc
+    C1 = frozenset([frozenset([el[2],el[3]]) for el in E1])
+    C2 = frozenset([frozenset([el[2],el[3]]) for el in E2])
 
-print(minC)
+    if len(C1) <= (C + 1):
+        if C1 not in critsets:
+            critsets.append(C1)
+    if len(C2) <= (C + 1):
+        if C2 not in critsets:
+            critsets.append(C2)
+
+    # if we reduce the size of the cut we need to clean our set of critical sets
+    if nmc < C:
+        C = nmc
+        for i in range(len(critsets) - 1, -1, -1):
+            Es = critsets[i]
+            if len(Es) > C+1:
+                del critsets[i]
+
+print(C)
+# print([len(el) for el in critsets])
+for i in range(len(critsets) - 1, -1, -1):
+            Es = critsets[i]
+            if len(Es) == C:
+                del critsets[i]
+print(len(critsets))
